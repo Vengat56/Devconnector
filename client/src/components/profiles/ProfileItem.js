e22e5e9438a52f39e985f2e9dc0ef90348049f22
unknown <vengat56@gmail.com>
2019-07-30 13:38:34 +0530

    updated

--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-//import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const ProfileItem = ({
@@ -20,9 +20,9 @@ const ProfileItem = ({
           {status} {company && <span> at {company}</span>}
         </p>
         <p className='my-1'>{location && <span>{location}</span>}</p>
-        {/*  <Link to={`/profile/${_id}`} className='btn btn-primary'>
+        <Link to={`/profile/${_id}`} className='btn btn-primary'>
           View Profile 
-        </Link> */}
+        </Link>
       </div>
       <ul>
         {skills.slice(0, 4).map((skill, index) => (
